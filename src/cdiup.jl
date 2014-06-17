6bfeb0ad72cf465fa7cb368b321a2b2ec3a0c4de
Joaquim <jmfluis@gmail.com>
2014-06-17 02:54:50 +0100

    Fix bad return type as picked by Clang.jl

--- src/cdiup.jl
+++ src/cdiup.jl
@@ -1,7 +1,8 @@
 # Julia wrapper for header: /Volumes/BOOTCAMP/programs/compa_libs/iup/include/cdiup.h
 # Automatically generated using Clang.jl wrap_c, version 0.0.0
 
-function cdContextIup()
-  ccall( (:cdContextIup, iup), Ptr{Cint}, (), )
-end
+@windows? (const libiupcd_ = "iupcd") : (const libiupcd_ = "libiupcd")  # Name of IUP shared lib.
 
+function cdContextIup()
+	ccall((:cdContextIup, libiupcd_), Ptr{cdContext}, (), )
+end
