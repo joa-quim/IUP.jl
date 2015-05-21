55bf508da7fcc86eb4e937c561240068d60e5463
Joaquim <jmfluis@gmail.com>
2015-05-21 02:16:53 +0100

    Update old declarations and add IupGetNativeHandleImage, IupGetImageNativeHandle, IupImageFromImImage

--- src/iupim.jl
+++ src/iupim.jl
@@ -3,10 +3,22 @@
 
 @windows? (const libiupim_ = "iupim") : (const libiupim_ = "libiupim")  # Name of shared lib.
 
+typealias Ihandle Void
 
-function IupLoadImage()
-	ccall((:IupLoadImage, libiupim_), Ptr{Cint}, (), )
+function IupLoadImage(file_name::String)
+	ccall((:IupLoadImage, libiupim_), Ptr{Ihandle}, (Ptr{Uint8},), file_name)
 end
-function IupSaveImage(ih::Ptr{Cint}, file_name::String, format::String)
-	ccall((:IupSaveImage, libiupim_), Cint, (Ptr{Cint}, Ptr{Uint8}, Ptr{Uint8}), ih, file_name, format)
+function IupSaveImage(ih::Ptr{Ihandle}, file_name::String, format::String)
+	ccall((:IupSaveImage, libiupim_), Cint, (Ptr{Ihandle}, Ptr{Uint8}, Ptr{Uint8}), ih, file_name, format)
 end
+
+function IupGetNativeHandleImage(handle::Ptr{Void})
+	ccall((:IupGetNativeHandleImage, libiupim_), Ptr{imImage}, (Ptr{Void},), handle)
+end
+function IupGetImageNativeHandle(image::Ptr{imImage})
+	ccall((:IupGetImageNativeHandle, libiupim_), Ptr{Void}, (Ptr{imImage},), image)
+end
+function IupImageFromImImage(image::Ptr{imImage})
+	ccall((:IupImageFromImImage, libiupim_), Ptr{Ihandle}, (Ptr{imImage},), image)
+end
+
